65ec9758e5248e4bdb747a747cb9ceabe0b75572
Sheikh Abdul Wahid <76596903+Sheikh-Abdul-Wahid@users.noreply.github.com>
2024-10-20 12:27:21 +0530

    Update Comments.py

--- Week 1/Concepts/Comments.py
+++ Week 1/Concepts/Comments.py
@@ -1,1 +1,9 @@
+# This is a single-line comment
 
+"""
+This is a multi-line comment
+used to explain code or provide additional information
+"""
+
+# Code execution starts here
+print("Hello, World!")
