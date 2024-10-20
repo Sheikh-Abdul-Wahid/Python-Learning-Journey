deee81febcdacd42827b1d61b3f4af4ba84ded92
Sheikh Abdul Wahid <76596903+Sheikh-Abdul-Wahid@users.noreply.github.com>
2024-10-20 12:14:01 +0530

    Add files via upload

---
+++ Week 1/Errors & Solutions/Error.py
@@ -0,0 +1,13 @@
+# Global variable
+
+x = 10
+
+def my_function(y):  # y is a local variable (function parameter)
+    z = 20  # z is a local variable
+    print("Inside function:", x, y, z)
+
+my_function(5)
+# Trying to access local variable outside function
+
+print(z)
+
