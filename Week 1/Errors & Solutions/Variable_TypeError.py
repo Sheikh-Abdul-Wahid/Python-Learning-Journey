d4a98851b59a470a43c6fd7f325d516bc2929c27
Sheikh Abdul Wahid <76596903+Sheikh-Abdul-Wahid@users.noreply.github.com>
2024-11-04 15:58:23 +0530

    Update and rename New_Error.py to Variable_TypeError.py

---
+++ Week 1/Errors & Solutions/Variable_TypeError.py
@@ -0,0 +1,16 @@
+a = 1
+b = 2
+c = "Abdul"
+print(a + b + c)
+
+TypeError: unsupported operand type(s) for +: 'int' and 'str'
+
+Solution: Convert Integers to Strings and then Concatenate
+
+a = 1
+b = 2
+c = "abdul"
+
+# Convert integers to strings before concatenation
+result = str(a) + str(b) + c
+print(result)  # Output: 12abdul
