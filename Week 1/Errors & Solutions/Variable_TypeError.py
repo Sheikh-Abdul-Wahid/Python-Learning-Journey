2abdf9e0718810ae08589eb464b8d680f646f8b2
Sheikh Abdul Wahid <76596903+Sheikh-Abdul-Wahid@users.noreply.github.com>
2024-11-04 20:42:08 +0530

    Update Variable_TypeError.py

--- Week 1/Errors & Solutions/Variable_TypeError.py
+++ Week 1/Errors & Solutions/Variable_TypeError.py
@@ -3,8 +3,7 @@ b = 2
 c = "Abdul"
 print(a + b + c)
 
-Output:
-TypeError: unsupported operand type(s) for +: 'int' and 'str'
+Output: TypeError: unsupported operand type(s) for +: 'int' and 'str'
 
 Solution: Convert Integers to Strings and then Concatenate
 
