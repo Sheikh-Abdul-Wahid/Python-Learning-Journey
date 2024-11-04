e245c184a627dc8a6ace653330bfdbb72623288d
Sheikh Abdul Wahid <76596903+Sheikh-Abdul-Wahid@users.noreply.github.com>
2024-11-04 20:35:58 +0530

    Update and rename integer_float_confusion.py to Assignment_Equality_Operator_Confusion.py

---
+++ Week 1/Errors & Solutions/Assignment_Equality_Operator_Confusion.py
@@ -0,0 +1,22 @@
+**Assignment Operator (=)**:
+
+# Assign value 10 to variable x
+x = 10
+print(x)  # Output: 10
+
+# Assign string "Hello" to variable greeting
+greeting = "Hello"
+print(greeting)  # Output: Hello
+
+**Equality Operator (==)**:
+
+# Compare integers
+x = 5
+y = 10
+print(x == y)  # Output: False
+
+# Compare strings
+greeting1 = "Hello"
+greeting2 = "Hello"
+print(greeting1 == greeting2)  # Output: True
+
