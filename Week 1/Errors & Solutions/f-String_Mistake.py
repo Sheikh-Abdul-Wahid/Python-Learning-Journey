265e48a152a0668a1389fec86b3fbf156c22e16a
Sheikh Abdul Wahid <76596903+Sheikh-Abdul-Wahid@users.noreply.github.com>
2024-11-05 18:18:50 +0530

    Update and rename boolean_usage_mistakes.py to f-String_Mistake.py

---
+++ Week 1/Errors & Solutions/f-String_Mistake.py
@@ -0,0 +1,11 @@
+# Mistake
+name = "John"
+age = 30
+print("I am {age} years old.") 
+# Output: I am {age} years old.
+
+#Solution
+name = "John"
+age = 30
+print(f"I am {age} years old.")
+# Output: I am 30 years old.
