f6d25fc54956bc7b47187ad0f7375c03d5d8a516
Sheikh Abdul Wahid <76596903+Sheikh-Abdul-Wahid@users.noreply.github.com>
2024-11-06 12:08:20 +0530

    Update List_Tuple_Immutability_Error.py

--- Week 1/Errors & Solutions/List_Tuple_Immutability_Error.py
+++ Week 1/Errors & Solutions/List_Tuple_Immutability_Error.py
@@ -2,12 +2,11 @@
 my_tuple = (1, 2, 3)
 
 # Trying to modify a tuple (this will raise an error)
-try:
-    my_tuple[0] = 10
-except TypeError as e:
-    print(f"Error: {e}")  # Output: Error: 'tuple' object does not support item assignment
+
+my_tuple [0] = 10
+print(my_tuple) # Output: Error: 'tuple' object does not support item assignment
 
 # Correct approach if you need a modifiable collection
 my_list = [1, 2, 3]
-my_list[0] = 10  # Lists can be modified
-print(my_list)  # Output: [10, 2, 3]
+my_list [0] = 10 # Lists can be modified
+print(my_list) # Output: [10, 2, 3]
