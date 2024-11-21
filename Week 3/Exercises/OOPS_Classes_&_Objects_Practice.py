b77b7f87139bb747126499828a7ad8768ddf51dd
Sheikh Abdul Wahid <76596903+Sheikh-Abdul-Wahid@users.noreply.github.com>
2024-11-21 21:11:15 +0530

    Create OOPS_Classes_&_Objects_Practice.py

---
+++ Week 3/Exercises/OOPS_Classes_&_Objects_Practice.py
@@ -0,0 +1,12 @@
+class Employee():
+    language = "Python"
+    salary = 1200000
+
+Abdul = Employee()
+Abdul.name = "Abdul"
+Abdul.language = "JAVA"
+print(Abdul.name, Abdul.language, Abdul.salary)
+
+rahul = Employee()
+rahul.name = "Rahul"
+print(rahul.name, rahul.salary, rahul.language)
