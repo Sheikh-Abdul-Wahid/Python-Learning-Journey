b877c1aa4e1206841c9e36fda4b9a9ec915ef051
Sheikh Abdul Wahid <76596903+Sheikh-Abdul-Wahid@users.noreply.github.com>
2024-11-21 22:59:32 +0530

    Update and rename error.py to File_Replace_Loop_Error.py

---
+++ Week 3/Errros & Solutions/File_Replace_Loop_Error.py
@@ -0,0 +1,25 @@
+# Error Code:
+
+words = ["Donkey", "Animal", "bad"]
+
+with open("file.txt") as f:   # Open the file and read the content
+    text = f.read()
+
+for word in words: 
+    newtext = text.replace(word, "#" * len(word))  # Replacing using the original text each time
+
+with open("file.txt", "w") as f: # Write the updated text back to the file
+    f.write(newtext)             # Only the last word gets replaced
+
+# Correct Code:
+
+words = ["Donkey", "Animal", "bad"]
+
+with open("file.txt") as f: # Open the file and read the content
+    text = f.read()
+
+for word in words: 
+    text = text.replace(word, "#" * len(word))  # Update text with the new replacement
+
+with open("file.txt", "w") as f: # Write the fully updated text back to the file
+    f.write(text)                # All words are replaced properly
