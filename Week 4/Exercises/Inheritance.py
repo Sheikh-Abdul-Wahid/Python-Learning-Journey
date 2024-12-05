15a07f127685967653c84e1a64d6949311e2d419
Sheikh Abdul Wahid <76596903+Sheikh-Abdul-Wahid@users.noreply.github.com>
2024-12-05 19:12:50 +0530

    Update Inheritance.py

--- Week 4/Exercises/Inheritance.py
+++ Week 4/Exercises/Inheritance.py
@@ -1,1 +1,26 @@
+class Employee: # Parent class
+    company = "IBM"
+    name = "Alex"
+    def show(self):
+        print(f"The employee name is {self.name} working in {self.company} company")
 
+# class Programmer():
+#     company = "IBM Info"
+#     def show(self):
+#         print(f"The employee name is {self.name} working in {self.company} company")
+    
+#     def showLang(self):
+#         print(f"The employee name is {self.name} and he is good with {self.language} language ")
+
+# Instead of Repeating the above code for def show(self) both the class we can use inheritance method
+
+class Programmer(Employee): # Child class (Inheritance) 
+    company = "IBM Info"
+    def showDetails(self): 
+        print(f"The employee name is {self.name} and he is working in {self.company} company") # Here it takes the self.name from Parent class Employee
+
+
+a = Employee()
+b = Programmer()
+print(a.company, b.company)
+b.showDetails() 
