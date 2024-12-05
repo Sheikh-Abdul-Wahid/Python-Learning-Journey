8f79da33645f2aa24f29b6fbc5fd638cd5016465
Sheikh Abdul Wahid <76596903+Sheikh-Abdul-Wahid@users.noreply.github.com>
2024-12-05 19:21:32 +0530

    Update super__init__method.py

--- Week 4/Exercises/super__init__method.py
+++ Week 4/Exercises/super__init__method.py
@@ -20,7 +20,8 @@ class Manager(Programmer):
         print(f"The employee works in {self.company} and his region is {self.region} who uses {self.language} language")
 
     def __init__(self):
-        super().__init__() # This will also call init of its parent class so no need to write __init__ of class Programmer, Also This method must be inside written inside def init of child class 
+        super().__init__() # This will also call init of its parent class so no need to write __init__ of class Programmer 
+                           # Also This method must be written inside def init of child class 
         print("Constructor of class Manager")
         
 c = Manager()
