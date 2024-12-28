17053c40778727df9fdc3c42f326c06a87ea0f0f
Sheikh Abdul Wahid <76596903+Sheikh-Abdul-Wahid@users.noreply.github.com>
2024-12-28 14:54:34 +0530

    Create join_method.py

---
+++ Week 6/Exercises/join_method.py
@@ -0,0 +1,18 @@
+l = ["apple","banana","donuts"]
+result = " or ".join(l)
+print(result)
+
+# Output:
+apple or banana or donuts
+
+# OR
+
+a = ["Abdul" , "Rohan" , "Alex" , "Allen" ]
+result = "-".join(a)
+final = "::".join(a)
+print(result)
+print(final)
+
+# Output:
+Abdul-Rohan-Alex-Allen   
+Abdul::Rohan::Alex::Allen
