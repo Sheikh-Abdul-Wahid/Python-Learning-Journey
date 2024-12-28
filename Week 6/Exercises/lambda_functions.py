e44fb955a27ae3348f01f6e1fe5b1b1c75f41bb1
Sheikh Abdul Wahid <76596903+Sheikh-Abdul-Wahid@users.noreply.github.com>
2024-12-28 14:31:59 +0530

    Create lambda_functions.py

---
+++ Week 6/Exercises/lambda_functions.py
@@ -0,0 +1,26 @@
+# Example 1:
+
+def square(n):
+    return n*n
+
+print(square(7))
+
+# Using lambda function
+
+square = lambda n : n*n
+print(square(7))
+
+# Example 2:
+
+def sum(a,b,c):
+    return a+b+c
+
+print(sum(5,10,7))
+
+# Using lambda function
+
+sum = lambda a,b,c : a+b+c
+print(sum(5,10,7))
+
+divide = lambda a,b : a/b
+print(divide(10,2))
