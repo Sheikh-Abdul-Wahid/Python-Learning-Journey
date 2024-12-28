e3adab3f00f4a63349a1ee59e85437f3a70b7b7a
Sheikh Abdul Wahid <76596903+Sheikh-Abdul-Wahid@users.noreply.github.com>
2024-12-28 14:59:05 +0530

    Create old_format_method.py

---
+++ Week 6/Exercises/old_format_method.py
@@ -0,0 +1,15 @@
+# Example 1: Automatic Field Numbering
+
+a = "{} is a good {}".format("Abdul","Boy")
+print(a)
+
+# Output:
+Abdul is a good Boy
+
+# Example 2: Manual Field Numbering
+
+a = "{1} is a good {0}".format("Programmer","Abdul")
+print(a)
+
+#Output: 
+Abdul is a good Programmer
